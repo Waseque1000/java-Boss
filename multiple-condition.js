dc4533b45f06e4b2167e46cf56a8287f9d790b4d
boss42wa <107799688+boss42wa@users.noreply.github.com>
2022-11-08 15:35:05 +0600

    multiple-conditon added

---
+++ multiple-condition.js
@@ -0,0 +1,11 @@
+// && = ai 2 ter mane holo 2 ta condition  same hoite heb 
+
+let graduation = true ;
+let salary = 40000 ;
+let car = 2 ;
+
+if (graduation == true && salary > 50000 || car > 1 ) {
+    console.log('eso biya kore feli ');
+}else{
+    console.log('tor kopale biye nai');
+}
