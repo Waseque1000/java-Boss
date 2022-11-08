a3a49e0949d441ddada40ecf36b3245330266e63
boss42wa <107799688+boss42wa@users.noreply.github.com>
2022-11-08 15:48:30 +0600

    Module 17 doneeee

---
+++ multi-stage-condition.js
@@ -0,0 +1,31 @@
+
+
+let myMoney = 15 ;
+
+let mango = 30 ;
+let banana = 20 ;
+let apple = 12 ;
+
+if (mango < myMoney) {
+    console.log('mango khabo');
+    
+}else if (banana < myMoney) {
+    console.log('Banana khabo');
+}else if ( apple < myMoney){
+    console.log('apple khabo');
+}
+
+
+// !  Nested
+
+let math = true;
+let bangla = true;
+
+if (math == true ) {
+    if (bangla == true ) {
+        
+    }else{
+        console.log(' eita ested er example');
+    }
+    
+}
