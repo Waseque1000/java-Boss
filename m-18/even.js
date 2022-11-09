581740ee5cf69059dbf0737ecba2c73b6c8e90f6
boss42wa <107799688+boss42wa@users.noreply.github.com>
2022-11-09 12:56:02 +0600

    odd, even number using while loops

---
+++ m-18/even.js
@@ -0,0 +1,10 @@
+// !   how can i find even number 
+
+let number = 0;
+
+while (number <= 20) {
+    console.log(number);
+    // number++;
+    number = number +2;
+
+}
---
+++ m-18/odd.js
@@ -0,0 +1,9 @@
+let number = 1;
+
+while (number < 20) {
+    console.log(number);
+     number = number + 2;
+
+    
+}
+  
