d31cc42dafffad1b78a981bc352ac09d7da22e58
boss42wa <107799688+boss42wa@users.noreply.github.com>
2022-11-10 16:02:47 +0600

    array loops done ... alhamdulillah

---
+++ m-18/array-loop.js
@@ -0,0 +1,23 @@
+/* for(i= 0; i < 10; i++){
+    console.log(i);
+} */
+
+
+// ! target : display every element of an array
+/* 
+var numbers = [45, 53, 67, 87, 81, 93, 188, 467, 5678];
+
+for( var i = 0; i < numbers.length ; i++){
+    var number = numbers[i];
+    console.log(number);
+}
+ */
+
+
+
+var items = ["bottle", 'mouse', 'glass', 'books'];
+for(var i = 0; i < items.length; i++){
+    var item = items[i];
+    console.log(item);
+}
+
---
+++ m-18/for.js
@@ -0,0 +1,42 @@
+
+// let roastGiven = 0;
+// while (roastGiven < 7) {
+//     console.log('vhaia roast den <:3');
+//     console.log(roastGiven);
+//     roastGiven++;
+    
+// }
+
+ //!   for loop
+//  for( var roastGiven = 0; roastGiven < 9 ; roastGiven++){
+//     console.log(' vhai roast den.....');
+
+//  }
+
+
+
+// ! simple version of for loop
+ /* console.log('simple for loop');
+ for (let i = 0; i < 8; i++) {
+   console.log(i);;
+    
+ } */
+
+
+/* 
+ for (let i = 0; i <=   10; i++) {
+    console.log(i);
+    
+ } */
+
+
+ for( var i = 0 ; i <= 20; i+=2){
+    console.log(i);
+
+ }
+
+
+ for( var i = 1 ; i <= 20; i+=2){
+    console.log(i);
+
+ }
