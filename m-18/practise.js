b12cf6eab73ec48e0956d8ed9e38a2ccc16468f3
boss42wa <107799688+boss42wa@users.noreply.github.com>
2022-11-13 15:06:54 +0600

    practise dome

---
+++ m-18/practise.js
@@ -0,0 +1,27 @@
+// let picnikFee = [ 45, 65, 23,87,34, 30];
+
+// picnikFee[3] = 100;
+// console.log(picnikFee); //! aita use kore array vitore kono kisu change korer jonno
+
+
+//  console.log(picnikFee.length);  // ? aita ude kora hoy kono array vitore kono gulo eliment aseo oite dekher jonno 
+//  console.log(picnikFee[3]); // * array vitore positio diyo / index diya kono eliment er man ber korer jonno 
+
+
+// let iphone = 90000;
+// let myBudget = 700000;
+
+// if (iphone < myBudget) {
+//     console.log(' ipnone kinbo futani marbo ');
+    
+// }else{
+//     console.log(' i will bye samsung... karon samsung phone onek xoss');}
+
+
+
+let numbers = [ 54, 67, 87, 90, 24, 37, 72];
+for( let i = 0; i < numbers.length; i++){
+    var number = numbers[i];
+    console.log(number);
+
+}
