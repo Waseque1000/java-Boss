e31b30909e6a76be3dae4048cd5876a50e8dd9c3
boss42wa <107799688+boss42wa@users.noreply.github.com>
2022-11-15 13:05:47 +0600

    done

---
+++ m-18/Importent-practise-2.js
@@ -0,0 +1,59 @@
+// var table = ['books', 'bottle', 'phone', 'pen', 'notebooks', 'tea'];
+
+//     // console.log(table.length);
+
+//     // console.log(table[1]);
+
+//     table[2] = 'This is boss';
+//     console.log(table);
+
+
+
+//     let  fee = [34, 45, 76, 123, 79, 97, 467,];
+//     let newfee = fee.indexOf(123);
+//     fee.push(200);
+//     console.log(fee);
+//     console.log(newfee);
+//     console.log(fee.length);
+
+
+ //! const fruits = ["Banana", "Orange", "Apple", "Mango"];
+ // ! fruits.push("Kiwi");
+ //  !console.log(fruits);
+
+ /* 
+  তোমার কাছে: ৮০০০০ টাকার বেশি হলে তুমি mac কিনবে, ৬০ টাকার বেশি হলে gaming ল্যাপটপ কিনবে, ৪০ হাজার টাকার বেশি হলে lenovo yoga কম্পিউটার কিনবে , ২০ হাজার টাকার বেশি হলে পুরান ল্যাপটপ কিনবে। না হয় তুমি মোবাইল দিয়ে কাজ চালাবে।  */
+
+  let mymoney = 84000 ;
+  let macBook = 80000;
+  let gamingLaptop = 60000;
+  let lenovoYoga = 40000;
+  let oldLeptop = 20000;
+  
+  
+  
+  if (mymoney > macBook) {
+      console.log(' i will buy a mac book'); 
+    } else if (mymoney > gamingLaptop) {
+        console.log('Naaahh Mac Book vlo na ,,, Jai akta gamming laptop kini');
+    } else if (mymoney > lenovoYoga) {
+        console.log(' Dhurrrrrr... Lenova upore kono laptop e  nai ');
+        
+    }else if (mymoney > oldLeptop) {
+        console.log(' shuvo vhai re laptop ta purai agun');
+    }
+    else {
+        console.log(' Vhai.... phoner upore kono kotha naiii');
+    }
+
+
+
+  //   if(mymoney > macBook){
+//     console.log( ' i will buy a mac book');
+//   } elseif (mymoney > gamingLaptop){
+//     console.log('Naaahh Mac Book vlo na ,,, Jai akta gamming laptop kini');
+//   }elseif(mymoney > lenovoYoga){
+//     console.log(' Dhurrrrrr... Lenova upore kono laptop nai ');
+//   }elseif(mymoney > oldLeptop) {
+//     console.log(' Vhai.... phoner upore kono kotha naiii');
+//   } 
