7834ec4cd3221d0c6c6119c92aa09da5f8a0d7f3
boss42wa <107799688+boss42wa@users.noreply.github.com>
2022-11-15 15:33:25 +0600

    alhamdulliah

--- m-18/Importent-practise-2.js
+++ m-18/Importent-practise-2.js
@@ -21,39 +21,103 @@
  // ! fruits.push("Kiwi");
  //  !console.log(fruits);
 
- /* 
-  তোমার কাছে: ৮০০০০ টাকার বেশি হলে তুমি mac কিনবে, ৬০ টাকার বেশি হলে gaming ল্যাপটপ কিনবে, ৪০ হাজার টাকার বেশি হলে lenovo yoga কম্পিউটার কিনবে , ২০ হাজার টাকার বেশি হলে পুরান ল্যাপটপ কিনবে। না হয় তুমি মোবাইল দিয়ে কাজ চালাবে।  */
 
-  let mymoney = 84000 ;
-  let macBook = 80000;
-  let gamingLaptop = 60000;
-  let lenovoYoga = 40000;
-  let oldLeptop = 20000;
+
+
+
+
+//  /* 
+//   তোমার কাছে: ৮০০০০ টাকার বেশি হলে তুমি mac কিনবে, ৬০ টাকার বেশি হলে gaming ল্যাপটপ কিনবে, ৪০ হাজার টাকার বেশি হলে lenovo yoga কম্পিউটার কিনবে , ২০ হাজার টাকার বেশি হলে পুরান ল্যাপটপ কিনবে। না হয় তুমি মোবাইল দিয়ে কাজ চালাবে।  */
+
+//   let mymoney = 84000 ;
+//   let macBook = 80000;
+//   let gamingLaptop = 60000;
+//   let lenovoYoga = 40000;
+//   let oldLeptop = 20000;
   
   
   
-  if (mymoney > macBook) {
-      console.log(' i will buy a mac book'); 
-    } else if (mymoney > gamingLaptop) {
-        console.log('Naaahh Mac Book vlo na ,,, Jai akta gamming laptop kini');
-    } else if (mymoney > lenovoYoga) {
-        console.log(' Dhurrrrrr... Lenova upore kono laptop e  nai ');
+//   if (mymoney > macBook) {
+//       console.log(' i will buy a mac book'); 
+//     } else if (mymoney > gamingLaptop) {
+//         console.log('Naaahh Mac Book vlo na ,,, Jai akta gamming laptop kini');
+//     } else if (mymoney > lenovoYoga) {
+//         console.log(' Dhurrrrrr... Lenova upore kono laptop e  nai ');
         
-    }else if (mymoney > oldLeptop) {
-        console.log(' shuvo vhai re laptop ta purai agun');
-    }
-    else {
-        console.log(' Vhai.... phoner upore kono kotha naiii');
-    }
+//     }else if (mymoney > oldLeptop) {
+//         console.log(' shuvo vhai re laptop ta purai agun');
+//     }
+//     else {
+//         console.log(' Vhai.... phoner upore kono kotha naiii');
+//     }
 
 
 
-  //   if(mymoney > macBook){
-//     console.log( ' i will buy a mac book');
-//   } elseif (mymoney > gamingLaptop){
-//     console.log('Naaahh Mac Book vlo na ,,, Jai akta gamming laptop kini');
-//   }elseif(mymoney > lenovoYoga){
-//     console.log(' Dhurrrrrr... Lenova upore kono laptop nai ');
-//   }elseif(mymoney > oldLeptop) {
-//     console.log(' Vhai.... phoner upore kono kotha naiii');
-//   } 
+/* 
+// ! আসকে আমার মন ভালো নেই -- এই কথা ৩৯ বার আউটপুট হিসেবে দেখাও। 
+
+let dailoge = 0;
+while ( dailoge < 40) {
+    console.log('আসকে আমার মন ভালো নেই');
+    dailoge++;
+    
+} */
+
+
+var picnikFee = [ 23, 45, 56, 67, 78, 43, 54];
+
+
+for (var  i = 0; i < picnikFee.length ; i++) {
+    var fee = picnikFee[i];
+   if( i > 50 ){
+    break;
+   }
+   // console.log(fee);
+    
+}
+
+
+
+// var picnikFee = [ 45, 56, 2224, 67, 98, 60,234,90, 41]
+// for (var i = 0; i < picnikFee.length; i++) {
+// var num = picnikFee[i];
+// if (num > 90) {
+//     continue;
+    
+// }
+// console.log(num);
+    
+// }
+
+
+
+
+
+
+var boss = 58 ;
+
+while (boss < 98) {
+    //console.log(boss);
+    boss = boss +2
+    
+}
+
+
+//*. একটা ফর লুপ চালাও। ৩০ থেকে ৮৬ পর্যন্ত। আর এই লুপ ৪৪ এ গেলে ব্রেক করবে। সেই জিনিস কোড করে দেখাও 
+
+var taka =  [30, 45, 50, 60, 70, 86, 90, 78, 79 ]
+for (var  i = 0; i < taka.length; i++) {
+   var vhai = taka[i];
+   if (vhai > 60) {
+    break;
+    
+   }
+   console.log(vhai);
+    
+}
+
+
+
+
+
+
