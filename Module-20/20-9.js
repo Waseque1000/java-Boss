1199d2614c91ba895a6aabde0c5bf76c98099aff
boss42wa <107799688+boss42wa@users.noreply.github.com>
2022-12-02 20:12:23 +0600

    alhamduliaaah

--- Module-20/20-9.js
+++ Module-20/20-9.js
@@ -1,1 +1,17 @@
-// ! Module Summary and Simple JavaScript debug
+let brazil = {
+  player1: "Naymer",
+  player2: "Antoni",
+  player3: "Vini ",
+  player4: "Casmiro",
+  player5: "Danilo",
+  player6: "Rafinha",
+  player7: "Silva",
+  player8: "Alison",
+};
+
+const keys = Object.keys[brazil];
+const value = Object.values[brazil];
+for (let key in brazil) {
+  const boss = brazil[key];
+  console.log(key, "=", boss);
+}
