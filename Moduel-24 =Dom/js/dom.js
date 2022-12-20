1fdff639ea0a83a9ee4e5a5c5a33c82bd13df9aa
boss42wa <107799688+boss42wa@users.noreply.github.com>
2022-12-20 20:34:26 +0600

    dommm

---
+++ Moduel-24 =Dom/js/dom.js
@@ -0,0 +1,10 @@
+// console.log("Hellow DOM");
+// console.log(document);
+// const student = {
+//   name: "abul",
+//   id: 24,
+//   study: function (timr) {
+//     console.log(time, "haa kortase ");
+//   },
+//   // study: "stude kortase but fail korbe mone hoy",
+// };
